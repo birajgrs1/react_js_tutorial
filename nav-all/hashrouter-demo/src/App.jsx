c25ab88949cd6e886ddeddfd2e37e53ae69669bc
birajgrs <birajghorasaine11@gmail.com>
2025-02-11 17:11:42 +0545

    Understanding about hashrouter in react

---
+++ nav-all/hashrouter-demo/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+import NavControl from "./components/NavControl";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <NavControl />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
